96f807282d59bb50ad098ed03357ee9c9490e586
thanhdc97 <thanh.dc@haposoft.com>
2021-09-17 19:16:37 +0700

    fix time

--- js/garden_loveheart_use.js
+++ js/garden_loveheart_use.js
@@ -1,8 +1,8 @@
 var offsetX = $("#loveHeart").width() / 2;
 var offsetY = $("#loveHeart").height() / 2 - 55;
 var together = new Date();
-together.setFullYear(2021, 3, 28);
-together.setHours(3); //地域时钟差
+together.setFullYear(2018, 2, 16);
+together.setHours(8); //地域时钟差
 together.setMinutes(0);
 together.setSeconds(0);
 together.setMilliseconds(0);
